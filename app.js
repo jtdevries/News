b0575706ca185eec8ea6707d889fa88f3d7cf86d
Thijs <44265614+jtdevries@users.noreply.github.com>
2019-03-20 14:15:53 +0100

    update

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const apiKey = '8348f21dc81c4e5083d3195573bc7ec4';
 const main = document.querySelector('main');
 const sourceSelector = document.querySelector('#sourceSelector');
-const defaultSource = 'polygon';
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+let defaultSource = 'polygon';
+let defaultPage = 1;
 
 window.addEventListener('load', async e => {
     updateNews();
     await updateSources();
     sourceSelector.value = defaultSource;
 
+    // Get selected Source
     sourceSelector.addEventListener('change', e =>{
         updateNews(e.target.value);
+    });
+
+    // Next Page
+    next.addEventListener('click', function(){
+        defaultPage++;
+        updateNews();
+    });
+    // Prev Page
+    prev.addEventListener('click', function(){
+        defaultPage--;
+        updateNews();
     });
 
     if('serviceWorker' in navigator) {
@@ -22,20 +37,23 @@ window.addEventListener('load', async e => {
     }
 });
 
+// Get API Key
 async function updateSources() {
     const res = await fetch(`https://newsapi.org/v2/sources?language=en&apiKey=${apiKey}`);
     const json = await res.json();
 
+    // Fill #sourceSelector with API source array
     sourceSelector.innerHTML = json.sources.map(src => `<option value="${src.id}">${src.name}</option>`).join('\n');
 }
 
-async function updateNews(source = defaultSource) {
-    const res = await fetch(`https://newsapi.org/v2/everything?language=en&pageSize=20&sources=${source}&apiKey=${apiKey}`);
+async function updateNews(source = defaultSource, page = defaultPage) {
+    const res = await fetch(`https://newsapi.org/v2/everything?language=en&pageSize=10&page=${page}&sources=${source}&apiKey=${apiKey}`);
     const json = await res.json();
 
     main.innerHTML = json.articles.map(createArticle).join('\n');
 }
 
+// Load Article
 function createArticle(article) {
     return `
     <div class="article">
